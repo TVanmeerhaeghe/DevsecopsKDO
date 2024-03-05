059c264d1d51373e406875ea38195cccf9a68b0d
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2024-03-05 10:06:21 +0100

    feat : added get for one list

--- API/models/listModels.js
+++ API/models/listModels.js
@@ -11,7 +11,30 @@ function getAllLists(callback) {
   });
 }
 
+function getListById(id, callback) {
+  connection.query(
+    "SELECT * FROM list WHERE id = ?",
+    [id],
+    (error, results, fields) => {
+      if (error) {
+        console.error(
+          "Erreur lors de la récupération de la liste par ID :",
+          error
+        );
+        callback(error, null);
+        return;
+      }
+      if (results.length === 0) {
+        callback(null, null);
+        return;
+      }
+      callback(null, results[0]);
+    }
+  );
+}
+
 module.exports = {
   connection: connection,
   getAllLists: getAllLists,
+  getListById: getListById,
 };
--- API/routers/listRoutes.js
+++ API/routers/listRoutes.js
@@ -15,4 +15,25 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req, res) => {
+  const listId = req.params.id;
+  listModels.getListById(listId, (error, list) => {
+    if (error) {
+      console.error(
+        "Erreur lors de la récupération de la liste par ID :",
+        error
+      );
+      res
+        .status(500)
+        .json({ error: "Erreur lors de la récupération de la liste par ID" });
+      return;
+    }
+    if (!list) {
+      res.status(404).json({ error: "Aucune liste trouvée avec cet ID" });
+      return;
+    }
+    res.json(list);
+  });
+});
+
 module.exports = router;
