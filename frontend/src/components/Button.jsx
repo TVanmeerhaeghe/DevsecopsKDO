fa3644d2186070e15936cf81e730e1fefff38663
DapGaetan <dapvril.gaetan@gmail.com>
2024-03-05 14:44:19 +0100

    fix: problem in react frontend

--- frontend/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({textcontent, onClick}) => {
-    
-    return(
-        <div><button onClick={onClick}>{textcontent}</button></div>
-    );
-}
-export default Button
