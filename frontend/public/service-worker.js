54818d84e9c262465b0d5f08e5f44554dd5b07af
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2024-04-30 12:19:01 +0200

    feat : added service worker for cache

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,46 @@
+/*eslint-disable no-restricted-globals*/
+
+const assetVersion = 1;
+const assetCacheKey = "assets-v" + assetVersion;
+const apiCacheKey = "api-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(assetCacheKey).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/src/app.css",
+        "/src/index.css",
+        "/src/app.js",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+
+  if (request.url.startsWith(self.location.origin)) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+});
+
+async function cacheFirst(request) {
+  const cacheResponse = await caches.match(request);
+  return cacheResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(apiCacheKey);
+
+  try {
+    const response = await fetch(request);
+    cache.put(request, response.clone());
+    return response;
+  } catch (error) {
+    const cachedResponse = await cache.match(request);
+    return cachedResponse || new Response("Hors ligne", { status: 503 });
+  }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 import GiftInListPage from "./pages/GiftInListPage";
@@ -7,6 +7,27 @@ import GiftPage from "./pages/GiftPage";
 import Header from "./components/Header";
 
 const App = () => {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then((registration) => {
+            console.log(
+              "Service Worker enregistré avec succès : ",
+              registration.scope
+            );
+          })
+          .catch((error) => {
+            console.log(
+              "Erreur lors de l'enregistrement du Service Worker : ",
+              error
+            );
+          });
+      });
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
